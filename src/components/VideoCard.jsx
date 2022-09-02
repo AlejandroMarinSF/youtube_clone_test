821129c5cfea52c6e07b49a045ce3e4292357d4f
AlejandroMarin <alejandromarin199901@gmail.com>
2022-09-02 11:11:19 -0300

    Adding video fetching functionality

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,12 @@
+import { CheckCircle } from '@mui/icons-material'
+import { Card, CardContent, CardMedia, Typography } from '@mui/material'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const VideoCard = () => {
+  return (
+    <div>VideoCard</div>
+  )
+}
+
+export default VideoCard
