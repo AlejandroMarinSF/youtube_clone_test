29fe6236844fb29fceb023198ca004c3dfddf99b
AlejandroMarin <alejandromarin199901@gmail.com>
2022-09-05 15:46:20 -0300

    Adding new functionalities to the project

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,18 +1,21 @@
-import { Box, Stack } from '@mui/material';
-import React from 'react'
-import {VideoCard, ChannelCard} from './';
+import React from "react";
+import { Stack, Box } from "@mui/material";
 
-const Videos = ({ videos }) => {
+import { ChannelCard, Loader, VideoCard } from "./";
+
+const Videos = ({ videos, direction }) => {
+  if(!videos?.length) return 'fddf';
+  
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
-        {videos.map((item, idx) => (
-            <Box key={idx}>
-                 {item.id.videoId && <VideoCard video={item} />}
-                 {item.id.channelId && <ChannelCard channelDetail={item} />}
-            </Box>
-        ))}
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} /> }
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
     </Stack>
-  )
+  );
 }
 
-export default Videos
+export default Videos;
